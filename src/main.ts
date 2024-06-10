bed7e1a911cef683ac522f5bb9811837e8092736
adebayo-moses <adebayomoses597@gmail.com>
2024-06-10 23:58:40 +0100

    hero section

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import './style.css'
+import './swiper.ts'
+
+declare global {
+  interface Window {
+    toggleTheme: () => void;
+  }
+}
+
+window.toggleTheme = function () {
+  if (document.body.classList.contains("dark")) document.body.classList.remove("dark");
+  else document.body.classList.add("dark");
+};
+// window.toggleTheme = function () {
+//   if (document.body.classList.contains("dark")) document.body.classList.remove("dark");
+//   else document.body.classList.add("dark");
+// };
